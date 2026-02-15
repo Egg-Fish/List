cacd7e2ba5268035f2f8eeb80a6ae8c23e253c7c
Egg-Fish <eugeniomanansala@gmail.com>
2026-02-15 16:24:13 +0800

    feat: Add constructors, getters, and private setters
    Length refers to the number of items in the list.
    Capacity refers to the number of items that the currently
    malloced buffer can take before needing a resize.

--- include/List.h
+++ include/List.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include <stddef.h>
+
 typedef struct List *List;
+
+List List_create(void);
+void List_destroy(List l);
+
+size_t List_getLength(List l);
+size_t List_getCapacity(List l);
--- src/List.c
+++ src/List.c
@@ -1,5 +1,45 @@
 #include "List.h"
 
+#include <stddef.h>
+#include <stdlib.h>
+
 struct List {
-    int test;
+    size_t length;
+    size_t capacity;
+    void **items;
 };
+
+List List_create(void) {
+    List l = malloc(sizeof(struct List));
+    size_t length = 0;
+    size_t capacity = 8;
+
+    l->length = length;
+    l->items = malloc(capacity * sizeof(void *));
+
+    return l;
+}
+
+void List_destroy(List l) {
+    if (l == NULL) {
+        free(l);
+        return;
+    }
+
+    free(l->items);
+    free(l);
+
+    return;
+}
+
+size_t List_getLength(List l) { return l->length; }
+void List_setLength(List l, size_t length) {
+    l->length = length;
+    return;
+}
+
+size_t List_getCapacity(List l) { return l->capacity; }
+void List_setCapacity(List l, size_t capacity) {
+    l->capacity = capacity;
+    return;
+}
